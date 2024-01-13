43befbb2448dc9affe6bf05abcab62878100e6ee
achmdabdillah <90201329+achmdabdillah@users.noreply.github.com>
2024-01-13 04:55:29 +0000

    tst

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const jost = Jost({
   weight: ['400', '700'],
   variable: '--font-jost',
 })
-
+// test
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
